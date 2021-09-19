3055bd6efeef766b9cba5b2e0c561751bf0e782e
Mikhail <qr.stronger@gmail.com>
2021-09-19 04:25:51 +0300

    Добавлено кэширование изображений

--- soccer/Services/ImageDataService.swift
+++ soccer/Services/ImageDataService.swift
@@ -10,6 +10,8 @@ import Foundation
 final class ImageDataService {
   static let shared = ImageDataService()
 
+  private let cache = NSCache<AnyObject, AnyObject>()
+
   private init() { }
 
   func imageData(urlString: String?) -> Data? {
@@ -18,7 +20,13 @@ final class ImageDataService {
       let urlString = urlString,
       let url = URL(string: urlString) else { return nil }
 
+    if let cachedData = cache.object(forKey: url as AnyObject) as? Data {
+      return cachedData
+    }
+
     let data = try? Data(contentsOf: url)
+
+    cache.setObject(data as AnyObject, forKey: url as AnyObject)
 
     return data
   }
